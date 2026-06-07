a216a30f38a4024ddd20968e76fa6f9bcc208695
asoobii-cyber <asoobii@gmail.com>
2026-06-07 22:47:40 +0900

    마크 이미지 따옴표 문법 오류 수정

--- app/page.tsx
+++ app/page.tsx
@@ -644,8 +644,8 @@ function HeroSection() {
           {/* ★ 고대병원 마크 (alt 글씨 제거 및 정상 매칭) */}
           <motion.img 
             variants={item}
-            src="/hospital-mark.png
-            alt="" 
+            src="/hospital-mark.png"
+            alt=""
             className="ek-hero-university-mark"
           />
 
